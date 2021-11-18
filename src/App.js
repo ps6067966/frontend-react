767c49c9fe01455eaf2131e14d5c89676f18b35e
Pratap Singh <ps6067966@gmail.com>
2021-11-18 15:11:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Super Cool!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Image Gallery</h1>
     </div>
   );
-}
+};
 
 export default App;
